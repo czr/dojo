df1ba80595e88176b93c927980687570436c3f63
Iain Campbell <iain.campbell@zpg.co.uk>
2016-05-09 13:35:18 +0100

    Added new Pontoon TDD exercise

---
+++ Pontoon/pontoon.py
@@ -0,0 +1,49 @@
+import unittest
+from pprint import pprint
+
+class DeckEmptyException(Exception):
+    pass
+
+class Game(object):
+    pass
+
+class CardDeck(object):
+    
+    def __init__(self):
+        self.deck = [1] * 52
+
+    def draw(self):
+        try:
+            self.deck.pop()
+        except(IndexError):
+            raise DeckEmptyException    
+
+class TestGame(unittest.TestCase):
+
+    def setUp(self):
+        self.game = Game()
+
+    def tearDown(self):
+        del self.game
+
+    def test_01(self):
+        pass
+
+class TestCardDeck(unittest.TestCase):
+
+    def setUp(self):
+        self.deck = CardDeck()
+
+    def tearDown(self):
+        del self.deck
+
+    def test_52_cards(self):
+        for _ in range(52):
+            card = self.deck.draw()
+
+        with self.assertRaises(DeckEmptyException):
+            self.deck.draw()
+
+
+if __name__ == '__main__':
+    unittest.main()
