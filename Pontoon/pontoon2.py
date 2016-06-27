8612556b753398067b046eda0b2ec3c224474d1d
Ashley Hindmarsh <ash+github@best-scarper.co.uk>
2016-06-27 13:10:25 +0100

    test for winner from multiple players... we leave a new failing test where one player has bust, because we know the 'highest_score' calulcation is not accounting for bust scores

--- Pontoon/pontoon2.py
+++ Pontoon/pontoon2.py
@@ -16,9 +16,37 @@ class GameHistoryTest(unittest.TestCase):
         winners = calculate_pontoon_winners(moves)
         self.assertItemsEqual(winners, [])
 
+    def test_two_players_second_win(self):
+        player1 = mock.MagicMock()
+        player2 = mock.MagicMock()
+        moves = [(player1, 10), (player2, 10), (player1, 7), (player2, 8)]
+        winners = calculate_pontoon_winners(moves)
+        self.assertItemsEqual(winners, [player2])
+
+    def test_two_players_first_passed_bust_second_win(self):
+        player1 = mock.MagicMock()
+        player2 = mock.MagicMock()
+        moves = [(player1, 10), (player2, 10), (player1, 7), (player2, 8),(player1, 5)]
+        winners = calculate_pontoon_winners(moves)
+        self.assertItemsEqual(winners, [player2])
+
 
 def calculate_pontoon_winners(moves):
-    return [moves[0][0]]
+    sums = {}
+    for move in moves:
+        sums[move[0]] = sums.get(move[0], 0) + move[1]
+    # here we have sums with all the totals per player:
+    # { player1 : 17, player2: 18 }
+    highest_score = max(sums.values())
+    if highest_score > 21:
+        return []
+    winners = []
+    for player in sums.keys():
+        if sums[player] == highest_score:
+            winners = [player]
+
+    return winners
+
 
 
 if __name__ == '__main__':
