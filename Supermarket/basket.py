c27253b1406d8707b63434553bbdbaa7da23f3b1
Colin Robertson <colin.robertson@zpg.co.uk>
2017-04-07 17:54:18 +0100

    Rewrite all tests for new discount methods, add tests for competing and repeated offers

--- Supermarket/basket.py
+++ Supermarket/basket.py
@@ -1,49 +1,4 @@
-def calculate_discounts(basket, offers_list, price_list):
-    total_discount = 0
-    for item, quantity in basket.items.items():
-        if item in offers_list:
-            offers = offers_list[item]
-            if type(offers) is not list:
-                offers = [offers]
-            best_discount = 0
-            for offer in offers:
-                discount_function = DISCOUNTS[offer]
-                discount, participating_items_count = discount_function(quantity, price_list[item])
-                if discount > best_discount:
-                    best_discount = discount
-            total_discount += best_discount
-
-    return total_discount
-
-def calculate_x_for_y(x, y, quantity, price):
-    participating_items_count = (quantity / x) * (x - y)
-    return (participating_items_count * price, participating_items_count)
-
-def calculate_discount_bogof(quantity, price):
-    return calculate_x_for_y(2, 1, quantity, price)
-
-def calculate_discount_three_for_two(quantity, price):
-    return calculate_x_for_y(3, 2, quantity, price)
-
-def calculate_discount_five_for_three(quantity, price):
-    return calculate_x_for_y(5, 3, quantity, price)
-
-def calculate_discount_two_for_one(basket, price_list):
-    applies_to = 'ItemA'
-    items = basket.items
-    if applies_to in items and items[applies_to] >= 2:
-        items[applies_to] = items[applies_to] - 2
-        return [ { 'basket': basket, 'discount': price_list[applies_to] } ]
-    else:
-        return []
-
-DISCOUNTS = {
-    'bogof': calculate_discount_bogof,
-    'three_for_two': calculate_discount_three_for_two,
-    'five_for_three': calculate_discount_five_for_three,
-}
-
-def calculate_price(basket, price_list, offers_list = {}):
+def calculate_price(basket, price_list, offers):
     # offers_list = { 'ItemA': 'bogof' }
     price = 0
 
@@ -51,18 +6,49 @@ def calculate_price(basket, price_list, offers_list = {}):
     for item, quantity in basket.items.items():
         price += price_list[item] * quantity
 
-    discount = calculate_discounts(basket, offers_list, price_list)
+    discount = calculate_discounts(basket, price_list, offers)
 
     return price - discount
+
+def calculate_discounts(basket, price_list, offers):
+    total_discount = 0
+
+    calculated_discounts = []
+    for offer in offers:
+        possibilities = offer(basket, price_list)
+        if possibilities:
+            best_discount = possibilities[0]['discount']
+            total_discount += best_discount
+
+    return total_discount
+
+def calculate_x_for_y(x, y, basket, price_list):
+    applies_to = 'ItemA'
+    items = basket.items
+    if items.get(applies_to, 0) >= x:
+        new_basket = Basket(items=basket.items.copy())
+        new_basket.items[applies_to] -= x
+        return [{
+            'basket': new_basket,
+            'discount': price_list[applies_to] * (x - y)
+        }]
+    else:
+        return []
+
+def calculate_discount_three_for_two(basket, price_list):
+    return calculate_x_for_y(3, 2, basket, price_list)
+
+def calculate_discount_five_for_three(basket, price_list):
+    return calculate_x_for_y(5, 3, basket, price_list)
+
+def calculate_discount_two_for_one(basket, price_list):
+    return calculate_x_for_y(2, 1, basket, price_list)
 
 class Basket(object):
     """docstring for Basket."""
     def __init__(self, items=None):
         super(Basket, self).__init__()
-        if items:
-            self.items = items
-        else:
-            self.items = {}
+        self.items = {} if items is None else items
 
     def __cmp__(self, other):
         return self.items == other.items
--- Supermarket/basket_test.py
+++ Supermarket/basket_test.py
@@ -1,6 +1,11 @@
 import unittest
 
-from basket import calculate_price, calculate_discount_two_for_one, Basket
+from basket import \
+    calculate_price, \
+    calculate_discount_two_for_one, \
+    calculate_discount_three_for_two, \
+    calculate_discount_five_for_three, \
+    Basket
 
 class BasketTest(unittest.TestCase):
 
@@ -8,74 +13,67 @@ class BasketTest(unittest.TestCase):
         basket = Basket()
         print(basket.items)
         price_list = {}
-        price = calculate_price(basket, price_list)
+        price = calculate_price(basket, price_list, [])
         self.assertEqual(price, 0)
 
     def test_one_item(self):
         basket = Basket({'ItemA': 1})
         price_list = { 'ItemA': 5 }
-        price = calculate_price(basket, price_list)
+        price = calculate_price(basket, price_list, [])
         self.assertEqual(price, 5)
 
     def test_two_items(self):
         basket = Basket({'ItemA': 1, 'ItemB': 1})
         price_list = { 'ItemA': 5, 'ItemB': 1 }
-        price = calculate_price(basket, price_list)
+        price = calculate_price(basket, price_list, [])
         self.assertEqual(price, 6)
 
     def test_repeated_items(self):
         basket = Basket({'ItemA': 2})
         price_list = { 'ItemA': 5 }
-        price = calculate_price(basket, price_list)
+        price = calculate_price(basket, price_list, [])
         self.assertEqual(price, 10)
 
     def test_weighed_item(self):
         basket = Basket({'WeighedItemA': 3})
         price_list = { 'WeighedItemA': 5 }
-        price = calculate_price(basket, price_list)
+        price = calculate_price(basket, price_list, [])
         self.assertEqual(price, 15)
 
     def test_bogof_with_one(self):
         basket = Basket({'ItemA': 1})
         price_list = { 'ItemA': 5 }
-        offers_list = { 'ItemA': 'bogof' }
-        price = calculate_price(basket, price_list, offers_list = offers_list)
+        offers_list = [calculate_discount_two_for_one]
+        price = calculate_price(basket, price_list, offers_list)
         self.assertEqual(price, 5)
 
     def test_bogof_with_two(self):
         basket = Basket({'ItemA': 2})
         price_list = { 'ItemA': 5 }
-        offers_list = { 'ItemA': 'bogof' }
-        price = calculate_price(basket, price_list, offers_list = offers_list)
+        offers_list = [calculate_discount_two_for_one]
+        price = calculate_price(basket, price_list, offers_list)
         self.assertEqual(price, 5)
 
     def test_bogof_with_three(self):
         basket = Basket({'ItemA': 3})
         price_list = { 'ItemA': 5 }
-        offers_list = { 'ItemA': 'bogof' }
-        price = calculate_price(basket, price_list, offers_list = offers_list)
+        offers_list = [calculate_discount_two_for_one]
+        price = calculate_price(basket, price_list, offers_list)
         self.assertEqual(price, 10)
 
     def test_three_for_two_with_three(self):
         basket = Basket({'ItemA': 3})
         price_list = { 'ItemA': 5 }
-        offers_list = { 'ItemA': 'three_for_two' }
-        price = calculate_price(basket, price_list, offers_list = offers_list)
+        offers_list = [calculate_discount_three_for_two]
+        price = calculate_price(basket, price_list, offers_list)
         self.assertEqual(price, 10)
 
     def test_five_for_three_with_five(self):
         basket = Basket({'ItemA': 5})
         price_list = { 'ItemA': 5 }
-        offers_list = { 'ItemA': 'five_for_three' }
-        price = calculate_price(basket, price_list, offers_list = offers_list)
+        offers_list = [calculate_discount_five_for_three]
+        price = calculate_price(basket, price_list, offers_list)
         self.assertEqual(price, 15)
-
-    def test_multiple_offers(self):
-        basket = Basket({'ItemA': 2})
-        price_list = { 'ItemA': 5 }
-        offers_list = { 'ItemA': [ 'bogof', 'bogof' ] }
-        price = calculate_price(basket, price_list, offers_list = offers_list)
-        self.assertEqual(price, 5)
 
     def test_discount_function(self):
         basket = Basket()
@@ -87,23 +85,21 @@ class BasketTest(unittest.TestCase):
         ]
         self.assertEqual(results, expected)
 
-    # TODO
-    # def test_competing_offers(self):
-    #     basket = [
-    #         ('ItemA', 1),
-    #         ('ItemA', 1),
-    #         ('ItemA', 1),
-    #         ('ItemA', 1),
-    #         ('ItemA', 1),
-    #         ('ItemA', 1),
-    #         ('ItemA', 1),
-    #         ('ItemA', 1),
-    #     ]
-    #     price_list = { 'ItemA': 5 }
-    #     offers_list = { 'ItemA': [ 'three_for_two', 'five_for_three' ] }
-    #     price = calculate_price(basket, price_list, offers_list = offers_list)
-    #     self.assertEqual(price, 25)
+    # TODO - refactor discount methods
+    def test_competing_offers(self):
+        basket = Basket({'ItemA': 8})
+        price_list = { 'ItemA': 5 }
+        offers_list = [calculate_discount_three_for_two,
+                       calculate_discount_five_for_three]
+        price = calculate_price(basket, price_list, offers_list)
+        self.assertEqual(price, 25)
 
+    def test_applying_offer_twice(self):
+        basket = Basket({'ItemA': 4})
+        price_list = {'ItemA': 5}
+        offers_list = [calculate_discount_two_for_one]
+        price = calculate_price(basket, price_list, offers_list)
+        self.assertEqual(price, 10)
         # {
         #     bogof: [ ItemA, ItemB ]
         #     buy_x_get_y: [ ItemC, ItemD ]
